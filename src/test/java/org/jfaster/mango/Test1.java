264164c5b1438864d783a84f32ff7700948b2a78
wuxi0823 <xwu12348@umd.edu>
2016-12-06 12:58:11 -0500

    Update Test1.java

--- src/test/java/org/jfaster/mango/Test1.java
+++ src/test/java/org/jfaster/mango/Test1.java
@@ -12,6 +12,19 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 public class Test1 {
+  @Test
+  public void test() throws Exception {
+  CombinedStat stat = CombinedStat.create();
   OperatorStat os = new OperatorStat();
   ExtendStat es = new ExtendStat(os);
+  MetaStat metaStat = stat.getMetaStat();
+  metaStat.setMethod(m);
+  metaStat.setOperatorType(OperatorType.UPDATE);
+  metaStat.setCacheable(true);
+  metaStat.setUseMultipleKeys(true);
+  metaStat.setCacheNullObject(true);
+  InitStat initStat = stat.getInitStat();
+  initStat.recordInit(1000);
+  OperatorStat operatorStat = stat.toOperatorStat();
+}
 }
