80272bfff906ede72208c3cb042ad94d9c8c0d01
wuxi0823 <xwu12348@umd.edu>
2016-12-06 02:57:22 -0500

    Add files via upload

---
+++ src/test/java/org/jfaster/mango/Test1.java
@@ -0,0 +1,16 @@
+ package org.jfaster.mango.plugin.stats;
+import org.jfaster.mango.annotation.DB;
+import org.jfaster.mango.annotation.SQL;
+import org.jfaster.mango.annotation.Sharding;
+import org.jfaster.mango.sharding.NotUseTableShardingStrategy;
+import org.jfaster.mango.stat.OperatorStat;
+import org.jfaster.mango.util.Strings;
+import org.jfaster.mango.util.ToStringHelper;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+public class Test1 {
+  ExtendStat a= new ExtendStat();
+}
