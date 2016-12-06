c869c7420bd0191f60d2c526ae432fa1e8d83be2
wuxi0823 <xwu12348@umd.edu>
2016-12-05 23:39:26 -0500

    Update tokenTest.java

--- src/test/java/org/jfaster/mango/tokenTest.java
+++ src/test/java/org/jfaster/mango/tokenTest.java
@@ -1,10 +1,1 @@
-package org.jfaster.mango.parser;
-import org.jfaster.mango.binding.InvocationContext;
-public class tokenTest{
-  @Test
-  public void testASTEQnode() throws Exception{
-    ASTEQNode i=ASTEQNode(1);
-    i.ASTEQNode(1,2);
-    i.evaluate(null);
-  }
-}
+
