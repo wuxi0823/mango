c3c27800f7e710f2ff6034f42e3c2b8d8814504e
wuxi0823 <xwu12348@umd.edu>
2016-12-05 23:26:08 -0500

    Add files via upload

---
+++ src/test/java/org/jfaster/mango/tokenTest.java
@@ -0,0 +1,10 @@
+package org.jfaster.mango.parser;
+import org.jfaster.mango.binding.InvocationContext;
+public class test1{
+  @Test
+  public void testASTEQnode() throws Exception{
+    ASTEQNode i=new ASTEQNode;
+    i.ASTEQNode(1,2);
+    i.evaluate(Null)
+  }
+}
