049c7093f7a75ec0b8f9e3be8333c85e21cd1688
wuxi0823 <xwu12348@umd.edu>
2016-12-09 20:13:48 -0500

    Add files via upload

---
+++ src/test/java/org/jfaster/mango/Log4J2LoggerTest.java
@@ -0,0 +1,178 @@
+package org.jfaster.mango.util.logging;
+import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.*;
+
+/**
+ * Created by wuxi on 16/12/5.
+ */
+public class Log4J2LoggerTest {
+
+    @Test
+    public void isTraceEnabled() throws Exception {
+        String name="xiwu";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        boolean a=b.isTraceEnabled();
+        assertThat(a, equalTo(false));
+
+    }
+
+    @Test
+    public void trace() throws Exception {
+        String name="xiwu";
+        Object o=null;
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.trace(name);
+        b.trace(name,o);
+
+    }
+
+    @Test
+    public void trace1() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.trace(name,one,two);
+    }
+
+    @Test
+    public void trace2() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        String three="where";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.trace(name,one,two,three);
+
+    }
+
+    @Test
+    public void trace3() throws Exception {
+
+    }
+
+    @Test
+    public void isDebugEnabled() throws Exception {
+        String name="xiwu";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        boolean a=b.isDebugEnabled();
+        assertThat(a, equalTo(false));
+
+    }
+
+    @Test
+    public void debug() throws Exception {
+        String name="xiwu";
+        Object o=null;
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.debug(name);
+        b.debug(name,o);
+    }
+
+    @Test
+    public void debug1() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.debug(name,one,two);
+
+    }
+
+    @Test
+    public void debug2() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        String three="where";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.debug(name,one,two,three);
+
+    }
+
+    @Test
+    public void isInfoEnabled() throws Exception {
+        String name="xiwu";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        boolean a=b.isInfoEnabled();
+        assertThat(a, equalTo(false));
+
+
+    }
+
+    @Test
+    public void info() throws Exception {
+        String name="xiwu";
+        Object o=null;
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.info(name);
+        b.info(name,o);
+
+
+    }
+
+    @Test
+    public void info1() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.info(name,one,two);
+    }
+
+    @Test
+    public void info2() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        String three="where";
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        b.info(name,one,two,three);
+    }
+
+
+    @Test
+    public void isWarnEnabled() throws Exception {
+
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        String three="where";
+        Object o=null;
+
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        boolean a=b.isWarnEnabled();
+        b.warn(name);
+        b.warn(name,o);
+        b.warn(name,one,two);
+        b.warn(name,one,two,three);
+        assertThat(a, equalTo(false));
+
+
+
+    }
+
+    @Test
+    public void isErrorEnabled() throws Exception {
+        String name="Xi Wu";
+        String one="select";
+        String two="join";
+        String three="where";
+        Object o=null;
+
+        Log4J2Logger b=new Log4J2Logger(LogManager.getLogger(name));
+        boolean a=b.isErrorEnabled();
+        b.error(name);
+        b.error(name,o);
+        b.error(name,one,two);
+        b.error(name,one,two,three);
+        assertThat(a, equalTo(true));
+
+    }
+
+}
